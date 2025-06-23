73bdbeed3be24f7c97d974876b05150725126cba
Gustavo Seabra <devguga87@gmail.com>
2025-06-23 17:38:13 -0300

    primeiro commit

---
+++ src/main/java/com/devsuperior/aula1/services/ShippingService.java
@@ -0,0 +1,17 @@
+package com.devsuperior.aula1.services;
+
+import com.devsuperior.aula1.entities.Order;
+
+public class ShippingService {
+    public double shipment(Order order){
+        double basic = order.getBasic();
+
+        if(basic < 100.0){
+            return 20.0;
+        } else if(basic < 200.0){
+            return 12.0;
+        } else {
+            return 0;
+        }
+    }
+}
